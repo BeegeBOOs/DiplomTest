25af72f8ae7ab7e47150db7bb1ece68109959f8c
BeegeBOOs <vitaliknb@gmail.com>
2022-05-31 01:10:47 +0300

    add tables:user,task

--- server/src/api/task/controllers/task.js
+++ server/src/api/task/controllers/task.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  *  task controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::task.task');
+module.exports = createCoreController("api::task.task", ({ strapi }) => ({
+  // Method 3: Replacing a core action
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.db.query("api::task.task").findOne({
+      where: { slug },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
